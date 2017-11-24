301d0460660eb5fb7ac1b14901dcf3a2062ceda4
Olga Lazarevich <helgalazarevich@gmail.com>
2017-11-24 15:54:34 +0200

    create function delete

--- js/index.js
+++ js/index.js
@@ -1,23 +1,42 @@
 "use strict"
 
-
-function tableCol (){
+function deleteTableColRow (){ 
     
+    var element = document.getElementById("table");
+    
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
 
-    for (var iRow = 0 ; iRow < 4 ; iRow++) {
+function createTableColRow (){
+    
+    deleteTableColRow ();
+    
+    var t = document.getElementById("table");
+
+    for (var iRow = 0 ; iRow < 10 ; iRow++) {
 
             var newRow = document.createElement('tr');
           
-               for (var iCol = 0; iCol < 3; iCol++) {
-                 newRow.innerHTML += '<td>Привет, мир!</td>';
+               for (var iCol = 0; iCol < 10; iCol++) {
+                 newRow.innerHTML += '<td>' + iCol +'</td>';
                 }
-         
-
-
-       table.insertBefore(newRow, table.children[1]);
+        
+    table.insertBefore(newRow, t.children[0]);
         }
-
-
-    
+    console.log(t.children[0]);
+    console.log(document.querySelectorAll('tr > td')[2].parentElement);
+    console.log(document.querySelectorAll('tr > td')[2].parentElement.parentElement);
+    console.log(document.querySelectorAll('tr > td')[2].parentElement.parentElement.parentElement);
+    console.log(document.querySelectorAll('tr > td')[2].parentElement.parentElement.parentElement.parentElement);
+    console.log(document.querySelectorAll('tr > td')[2].parentElement.parentElement.parentElement.parentElement.parentElement);
+    console.log(document.querySelectorAll('tr > td')[2].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement);
 }
+
+
+
+
+
+
 
